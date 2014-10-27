19968cd742022e976a7b089e965d557ffb186aa1
miratepuffin <ben_steer@hotmail.co.uk>
2014-10-27 01:00:08 +0000

    Added the full bar/beat/tatum/segment examples
    Bars beats and tatums are all TimeEvents and have the exact same
    methods. Will add full comments tomorrow to explain what everything is

--- EchoNestObject/src/test.java
+++ EchoNestObject/src/test.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+
 import com.echonest.api.v4.EchoNestException;
+import com.echonest.api.v4.Segment;
+import com.echonest.api.v4.TimedEvent;
 
 
 public class test {
@@ -57,6 +61,31 @@ public class test {
 				System.out.println("Beat 1: Beat start time " +beat1[0]+ " beat duration: " + beat1[1]+" beat confidence "+beat1[2]);
 				System.out.println("Tatum 1: Tatum start time " +tatum1[0]+ " tatum duration: " + tatum1[1]+" tatum confidence "+tatum1[2]);
 				
+				ArrayList<TimedEvent> bars =  meta.getBars();
+				bars.get(1).getConfidence();
+				bars.get(1).getDuration();
+				bars.get(1).getStart();
+				ArrayList<Segment> segments = meta.getSegments();
+				//segments are slightly different, have pitches, timbre (the character or quality of a musical sound or voice as distinct from its pitch and intensity.
+				//"trumpet has different timbres to saxophone")
+				segments.get(1).getConfidence();
+				System.out.println("length of segment 40: " +segments.get(40).getDuration());
+				System.out.println("max loudness of segment 40: " +segments.get(40).getLoudnessMax());
+				System.out.println("time of max loudess of segment 40: " +segments.get(40).getLoudnessMaxTime());
+				System.out.println("start loudness time of segment 40: " +segments.get(1).getLoudnessStart()); // I need to look into what the hell this is
+				double[] pitches = segments.get(40).getPitches();
+				segments.get(1).getStart();
+				double[] timbres = segments.get(40).getTimbre();
+				
+				for(int i =0;i<pitches.length;i++){
+					int x = i+1;
+					System.out.println("pitch "+ x + "from segment 40:     " + pitches[i]);
+				}
+				for(int i =0;i<timbres.length;i++){
+					int x = i+1; 
+					System.out.println("timbre "+ x + "from segment 40:     " + timbres[i]);
+				}
+				//will add notes on all of these tomorrow 
 			}
 		} catch (EchoNestException e) {
 			// TODO Auto-generated catch block
